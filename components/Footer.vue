f681ae84d22aa9ec888d159a7d3cff3da79e5a57
Ditaaa <113565619+ditanursabila@users.noreply.github.com>
2025-01-21 12:22:02 +0700

    update pages

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,59 +1,92 @@
 <template>
-    <div>
-        <!-- ========== FOOTER ========== -->
-        <footer class="mt-auto bg-gray-900 w-full">
-            <div class="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 lg:pt-20 mx-auto">
-                <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-32">
-                    <div class="col-span-full lg:col-span-2">
-                        <a class="flex-none text-xl font-semibold text-white focus:outline-none focus:opacity-80"
-                            href="#" aria-label="Brand">Tentang</a>
-                        <p
-                            class="inline-flex mt-3 gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200">
-                            LPK Tsukuba Indonesia didirikan pada bulan Agustus 2024
-                            di Ciawi, Kabupaten Tasikmalaya, Jawa Barat Indonesia.
-                            dan telah mendapatkan izin resmi dari pemerintah Indonesia.
-                        </p>
-                    </div>
-                    <div class="col-span-full lg:col-span-2">
-                        <a class="flex-none text-xl font-semibold text-white focus:outline-none focus:opacity-80"
-                            href="#" aria-label="Brand">LPK TSUKUBA INDONESIA</a>
-                        <p>
-                            <a class="inline-flex mt-3 gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
-                                href="#">Perum Puri Ciawi Kencana Blok J3, Pasirhuni, Kec. Ciawi, Kab. Tasikmalaya, Jawa
-                                Barat 46156</a>
-                        </p>
-                        <p>
-                            <a class="inline-flex mt-3 gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
-                                href="#">tsukubaindonesia@gmail.com</a>
-                        </p>
-                    </div>
-                    <div class="col-span-1">
-                        <h4 class="font-semibold text-gray-100">Media Sosial</h4>
-                        <div class="mt-3 grid space-y-3">
-                            <p>
-                                <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
-                                    href="#">Instagram</a>
-                            </p>
-                            <p>
-                                <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
-                                    href="#">Tiktok</a>
-                            </p>
-                            <p>
-                                <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
-                                    href="#">whatsapp</a>
-                            </p>
+    <!-- ========== FOOTER ========== -->
+    <footer class="mt-auto bg-gray-900 w-full">
+        <div class="mt-5 w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 lg:pt-20 mx-auto">
+            <div class="grid grid-cols-12 md:grid-cols-12 gap-10 justify-center">
+                <div class="col-span-12 md:col-span-7">
+                    <div class="space-y-6">
+                        <div>
+                            <div>
+                                <a class="flex-none text-xl font-semibold text-white focus:outline-none focus:opacity-80"
+                                    href="#" aria-label="Brand">Tentang</a>
+                                <p
+                                    class="mt-3 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200">
+                                    LPK Tsukuba Indonesia didirikan pada bulan Agustus 2024
+                                    di Ciawi, Kabupaten Tasikmalaya, Jawa Barat Indonesia.
+                                    dan telah mendapatkan izin resmi dari pemerintah Indonesia.
+                                </p>
+                                <div class="mt-4">
+                                    <a class="flex-none text-xl font-semibold text-white focus:outline-none focus:opacity-80"
+                                        href="#" aria-label="Brand">LPK TSUKUBA INDONESIA</a>
+                                    <p>
+                                        <a class="inline-block mt-3 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                            href="#">Perum Puri Ciawi Kencana Blok J3, Pasirhuni, Kec. Ciawi, Kab.
+                                            Tasikmalaya, Jawa Barat 46156</a>
+                                    </p>
+                                    <p>
+                                        <a class="inline-block mt-3 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                            href="#">tsukubaindonesia@gmail.com</a>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="mt-5 sm:mt-12 grid gap-y-2 sm:gap-y-0 sm:flex sm:justify-between sm:items-center">
-                    <div class="flex justify-between items-center">
-                        <p class="text-sm text-gray-400">
-                            © 2025 LPK TSUKUBA INDONESIA All Right Reserved
+
+                <div class="col-span-6 md:col-span-2">
+                    <h4 class="font-semibold text-gray-100">Quick Link</h4>
+                    <div class="mt-3 space-y-3">
+                        <p>
+                            <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                href="#">Home</a>
+                        </p>
+                        <p>
+                            <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                href="/persyaratan">Persyaratan</a>
+                        </p>
+                        <p>
+                            <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                href="/galeri">Galeri</a>
+                        </p>
+                        <p>
+                            <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                href="/kontak">Kontak</a>
+                        </p>
+                        <p>
+                            <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                href="/tentang">Tentang</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-span-6 md:col-span-3">
+                    <h4 class="font-semibold text-gray-100">Media Sosial</h4>
+                    <div class="mt-3 space-y-3">
+                        <p>
+                            <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                href="#">Instagram</a>
+                        </p>
+                        <p>
+                            <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                href="#">Tiktok</a>
+                        </p>
+                        <p>
+                            <a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 focus:outline-none focus:text-gray-200"
+                                href="#">whatsapp</a>
                         </p>
                     </div>
                 </div>
             </div>
-        </footer>
-        <!-- ========== END FOOTER ========== -->
-    </div>
+
+            <div
+                class="mt-5 sm:mt-12 grid gap-y-2 sm:gap-y-0 sm:flex sm:justify-center md:items-center sm:items-center text-center">
+                <div class="flex justify-center items-center">
+                    <p class="text-sm text-center text-gray-400">
+                        &copy; 2025 LPK TSUKUBA INDONESIA All Right Reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ========== END FOOTER ========== -->
 </template>
--- components/Kontak.vue
+++ components/Kontak.vue
@@ -107,7 +107,7 @@
                   class="w-full px-4 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                   rows="4"></textarea>
               </div>
-              <div class="flex ms-7">
+              <div class="flex">
                 <button type="submit"
                   class="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-transform duration-300 transform hover:scale-105">
                   Kirim

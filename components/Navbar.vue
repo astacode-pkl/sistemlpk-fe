d7829d17f2931b4ed065c8824f77709a7ebf3227
Ditaaa <113565619+ditanursabila@users.noreply.github.com>
2025-01-22 22:26:51 +0700

    update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -19,7 +19,7 @@
                     <!-- Collapse Button -->
                     <div class="md:hidden">
                         <button type="button"
-                            class="hs-collapse-toggle relative size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
+                            class="hs-collapse-toggle relative size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
                             id="hs-header-classic-collapse" aria-expanded="false" aria-controls="hs-header-classic"
                             aria-label="Toggle navigation" data-hs-collapse="#hs-header-classic">
                             <svg class="hs-collapse-open:hidden size-4" xmlns="http://www.w3.org/2000/svg" width="24"
@@ -44,7 +44,7 @@
                 <!-- End Logo w/ Collapse Button -->
                 <!-- Collapse -->
                 <div id="hs-header-classic"
-                    class="hs-collapse hidden overflow-hidden bg-white w-full lg:bg-transparent rounded-lg px-10 pt-3 lg:pt-0 lg:px-0 transition-all duration-300 basis-full grow md:block"
+                    class="hs-collapse hidden overflow-hidden bg-white w-full lg:bg-transparent rounded-lg px-10 pt-3 mt-5 lg:mt-0 lg:pt-0 lg:px-0 transition-all duration-300 basis-full grow md:block"
                     aria-labelledby="hs-header-classic-collapse">
                     <div
                         class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300">
@@ -53,40 +53,40 @@
                             <Nuxt-link
                                 class="p-2 flex items-center text-sm  hover:text-blue-600 focus:outline-none focus:text-blue-600"
                                 to="/" aria-current="page" :class="{
-                'text-white': !isScrolled,
-                'text-gray-800': isScrolled
+                'md:text-white': !isScrolled,
+                'md:text-gray-800': isScrolled
             }">
                                 Home
                             </Nuxt-link>
                             <Nuxt-link
                                 class="p-2 flex items-center text-sm text-gray-800 hover:text-blue-600 focus:outline-none focus:text-blue-600"
                                 to="/persyaratan" :class="{
-                'text-white': !isScrolled,
-                'text-gray-800': isScrolled
+                'md:text-white': !isScrolled,
+                'md:text-gray-800': isScrolled
             }">
                                 Persyaratan
                             </Nuxt-link>
                             <Nuxt-link
                                 class="p-2 flex items-center text-sm text-gray-800 hover:text-blue-600 focus:outline-none focus:text-blue-600"
                                 to="/galeri" :class="{
-                'text-white': !isScrolled,
-                'text-gray-800': isScrolled
+                'md:text-white': !isScrolled,
+                'md:text-gray-800': isScrolled
             }">
                                 Galeri
                             </Nuxt-link>
                             <Nuxt-link
                                 class="p-2 flex items-center text-sm text-gray-800 hover:text-blue-600 focus:outline-none focus:text-blue-600"
                                 to="/kontak" :class="{
-                'text-white': !isScrolled,
-                'text-gray-800': isScrolled
+                'md:text-white': !isScrolled,
+                'md:text-gray-800': isScrolled
             }">
                                 Kontak
                             </Nuxt-link>
                             <Nuxt-link
                                 class="p-2 flex items-center text-sm text-gray-800 hover:text-blue-600 focus:outline-none focus:text-blue-600"
                                 to="/tentang" :class="{
-                'text-white': !isScrolled,
-                'text-gray-800': isScrolled
+                'md:text-white': !isScrolled,
+                'md:text-gray-800': isScrolled
             }">
                                 Tentang
                             </Nuxt-link>
